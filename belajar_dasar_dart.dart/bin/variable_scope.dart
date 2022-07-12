ed1175e496f9772c943796324d06e583de8540f9
lexynotfound <36406297+lexynotfound@users.noreply.github.com>
2022-07-12 23:05:16 +0700

    Add files via upload

--- belajar_dasar_dart.dart/bin/variable_scope.dart
+++ belajar_dasar_dart.dart/bin/variable_scope.dart
@@ -16,9 +16,16 @@ void main() {
 /*
   Pada kode di atas variabel discount masih bisa diakses dari dalam kode if karena masih berada di dalam satu scope fungsi main(). 
   Bagaimana jika Anda ingin memisahkan kode di atas menjadi dua fungsi untuk menghitung diskonnya?
+
+
+  Variabel discount dideklarasikan pada fungsi checkDiscount() sehingga memiliki scope pada fungsi tersebut dan menyebabkan eror pada fungsi main(). 
+  Maka untuk mengatasinya kita tetap perlu membuat variabel di kedua fungsi.
+  Selain berada dalam lingkup fungsi, suatu variabel juga bisa menjadi variabel global, yaitu variabel yang dideklarasikan di luar blok kode apa pun. 
+  Variabel ini bisa diakses di mana pun selama masih berada di berkas yang sama.
+  Variabel juga dapat memiliki scope yang sespesifik mungkin hingga ke level control flow.
  */
 
-  num checkDiscount(num price) {
+  num checkDisount(num price) {
     num discount = 0;
     if (price >= 100000) {
       discount = 10 / 100 * price;
